258cd5d70531222ac0deb656d455841437b6146f
jayu-108 <barijayu@gmail.com>
2023-01-25 15:29:05 +0530

    upto vido 16

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,16 +1,21 @@
 import { useState } from 'react';
 import './App.css';
 import Alert from './components/Alert';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  // link
+} from "react-router-dom";
 
 function App() {
-  const[mode, setMode] = useState('light');
-  const[alert, setAlert] = useState(null);
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
 
-  const showAlert = (message, type)=>{
+  const showAlert = (message, type) => {
     setAlert({
       msg: message,
       type: type
@@ -20,46 +25,52 @@ function App() {
     }, 1500);
   }
 
-  const toggleMode = ()=>{
-    if(mode === 'light'){
+  const toggleMode = () => {
+    if (mode === 'light') {
       setMode('dark')
       document.body.style.backgroundColor = '#042743'
       showAlert("Dark mode has been enabled", "success")
+      document.title = "Textutils - Dark Mode"
     }
-    else{
+    else {
       setMode('light')
       document.body.style.backgroundColor = 'white'
       showAlert("Light mode has been enabled", "success")
+      document.title = "Textutils - Light Mode"
     }
   }
 
-  const toggleModeRed = ()=>{
+  const toggleModeRed = () => {
     setMode('dark')
     document.body.style.backgroundColor = '#dc3545'
     showAlert("Red Theme has been enabled", "success")
   }
-  
-  const toggleModeYel = ()=>{
+
+  const toggleModeYel = () => {
     setMode('dark')
     document.body.style.backgroundColor = '#ffc107'
     showAlert("Yellow Theme has been enabled", "success")
   }
 
-  const toggleModeGreen = ()=>{
+  const toggleModeGreen = () => {
     setMode('dark')
     document.body.style.backgroundColor = '#198754'
     showAlert("Green Theme has been enabled", "success")
   }
-  
+
   return (
     <>
-      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} toggleModeRed={toggleModeRed}
-      toggleModeYel={toggleModeYel} toggleModeGreen={toggleModeGreen}/>
-      <Alert alert={alert}/>
-      <div className="container my-3">
-        <TextForm  showAlert={showAlert} heading="Enter Text To Analyze Below" mode={mode}/>
-        {/* <About /> */}
-      </div>
+      <Router>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} toggleModeRed={toggleModeRed}
+          toggleModeYel={toggleModeYel} toggleModeGreen={toggleModeGreen} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route exact path="/about" element={<About aboutText="About Us"/>} />
+            <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter Text To Analyze Below" mode={mode} />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
